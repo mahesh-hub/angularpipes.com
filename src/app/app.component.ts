e50bb68959e166fc5d5f4925b63f0e8077cf02ff
mahesh-hub <mahesh8978051591@gmail.com>
2023-07-30 22:08:50 +0530

    first commit -commiting all files to the reposetry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularpipes';
+  title: string = 'angularpipes';
+  count: number = 234567;
+  dcValue: number = 3.6578;
+  price: number = 99.99;
+  toDay: Date = new Date();
+  postObj: object = {
+    id: 1,
+    postTitle: "post 1"
+  }
+  objArray: Array<string> = [
+    "post 1",
+    "post 2",
+    "post 3",
+    "post 4",
+    "post 5",
+    "post 6",
+  ]
+
+  userdetails = {
+    name: "user 1",
+    city: "newyork",
+    country: "us",
+  }
+
+  dummyText:string = "Copying text from VS Code includes the formatting which is nice until you paste into a program that is not very format aware - for me this was Gmail and pasting would always include the formatted text but I just wanted plain-text. Work arounds were annoying so I thought I'd try making an extension for it."
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppendPipe } from './pipes/append.pipe';
+import { AppendCLIPipe } from './pipes/append-cli.pipe';
+import { SummaryPipe } from './pipes/summary.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppendPipe,
+    AppendCLIPipe,
+    SummaryPipe
+
   ],
   imports: [
     BrowserModule,
